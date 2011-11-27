e88b9ffa370a3a22fa28ff2780317b44ffee994b
sarunnio <sarunnio@hotmail.com>
2011-11-27 18:04:56 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soccerweb::Application.config.secret_token = 'd633764f3decdd717a138dd4142d16dec57568d3f5b762e03b03e11c63326daf0f49bf88162b816745da94443817a7a00d89d90dbbfb1a2cd34c55c1c3034a1b'
