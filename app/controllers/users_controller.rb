0e7478487af0f044ca459bc0bad0a3bda8ffb80d
sarunnio <sarunnio@hotmail.com>
2012-01-19 07:26:31 +0700

    save state

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
     @title = "Sign up"
   end
+  
+  def create
+     @user = User.new(params[:user])
+    
+     if @user.save
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+        
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
 end
---
+++ person.rb.bak
@@ -0,0 +1,19 @@
+class Person 
+	attr_accessor :name 
+
+
+	def write_name
+		name = "test"
+		
+	end
+
+	def read_name
+		name
+	end
+
+end
+
+person = Person.new
+person.write_name
+puts person.name
+puts person.read_name
